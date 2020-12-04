eeb2bc20b8020899289aa3bde368bec4b0e6995e
aniruddhsinghal5 <62196589+aniruddhsinghal5@users.noreply.github.com>
2020-12-05 00:15:12 +0530

    Update run.php

--- run.php
+++ run.php
@@ -1,14 +1,14 @@
 <?php
 require "./cc.class.php";
 echo("###############################################\n");
-echo("#     Auto Generate + Checker CC Extrap       #\n");
+echo("#     Automatic Card Generator + CCN CHECKER       #\n");
 echo("###############################################\n");
-echo("# Create By : Kyuoko                          #\n");
+echo("# Created By : Aniruddh                          #\n");
 echo("# Code : PHP                                  #\n");
-echo("# Thx To UKL-TEAM, GFS-TEAM, AND YOU          #\n");
+echo("# Telegram - @aniruddhsinghal5          #\n");
 echo("###############################################\n");
 echo "[+] BIN : "; $bin = trim(fgets(STDIN));
-echo "[+] Check Status Valid ? 1/0 : "; $check = trim(fgets(STDIN));
-echo "[+] Amount/Jumlah : "; $amount = trim(fgets(STDIN));
+echo "[+] Check Cards too? (1-Yes,0-No) : "; $check = trim(fgets(STDIN));
+echo "[+] Number of cards to Check : "; $amount = trim(fgets(STDIN));
 $new = new CC($bin, $check, $amount);
 echo $new->Execute();
